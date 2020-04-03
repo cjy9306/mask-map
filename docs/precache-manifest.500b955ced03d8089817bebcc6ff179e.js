8222fff46ba8fca94b53da01cc1bd0d9aee85a4e
JAEYONG CHOI <richson1@naver.com>
2020-04-03 19:10:59 +0900

    add files for github pages

---
+++ docs/precache-manifest.500b955ced03d8089817bebcc6ff179e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3353a1f5cb4361259dce3c43c53cc68e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ec1992ab9874599ec72",
+    "url": "/static/css/main.54260386.chunk.css"
+  },
+  {
+    "revision": "425671145fcd2f1003da",
+    "url": "/static/js/2.342124df.chunk.js"
+  },
+  {
+    "revision": "215343f0487b3b92d1cde8daa4db5bf5",
+    "url": "/static/js/2.342124df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec1992ab9874599ec72",
+    "url": "/static/js/main.c4e194de.chunk.js"
+  },
+  {
+    "revision": "9d6f27ef6eab9a306a41",
+    "url": "/static/js/runtime-main.6c0bb28d.js"
+  }
+]);
